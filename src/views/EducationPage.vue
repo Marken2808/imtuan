a8e00744c75f14351808c85e459af781074c3502
Marken <nctuan.ityu@gmail.com>
2021-05-16 22:24:47 +0100

    new

--- src/views/EducationPage.vue
+++ src/views/EducationPage.vue
@@ -6,40 +6,45 @@
         <p>Education</p>
 
         <b-container fluid>
-          <b-row class="justify-content-center" cols="3">
-            <b-col cols="1">
-              <b-list-group v-b-scrollspy:listgroup-ex>
+          <b-row class="justify-content-center align-items-center" cols="2" >
+            <b-col cols="2" >
+              <b-button-group vertical v-b-scrollspy:testButton id="buttonGroup" class="justify-content-center align-items-center" >
                 
-                <b-list-group-item href="#list-item-1">
-                  <b-icon icon="circle"/>
-                </b-list-group-item>
+                <b-button pill v-for="(btn, idx) in buttons" :key="idx" :pressed.sync="btn.state" variant="white" :href="btn.href">
+                  <b-icon icon="circle" />
+                </b-button>
                 <hr>
 
-                <b-list-group-item href="#list-item-2">
-                  <b-icon icon="circle"/>
-                </b-list-group-item>
+
+                <!-- <b-button pill href="#list-item-2">
+                  <b-icon icon="circle" />
+                </b-button>
                 <hr>
 
-                <b-list-group-item href="#list-item-3">
-                  <b-icon icon="circle"/>
-                </b-list-group-item>
+
+                <b-button pill href="#list-item-3">
+                  <b-icon icon="circle" />
+                </b-button>
                 <hr>
 
-                <b-list-group-item href="#list-item-4">
-                  <b-icon icon="circle"/>
-                </b-list-group-item>
+
+                <b-button pill href="#list-item-4">
+                  <b-icon icon="circle" />
+                </b-button>
                 <hr>
 
-                <b-list-group-item href="#list-item-5">
-                  <b-icon icon="circle"/>
-                </b-list-group-item>
-                <hr>
 
-              </b-list-group>
+                <b-button pill href="#list-item-5">
+                  <b-icon icon="circle" />
+                </b-button>
+                <hr> -->
+
+              </b-button-group>
             </b-col>
 
             <b-col cols="7" md="8" lg="9">
-              <div id="listgroup-ex" style="position:relative; overflow-y:auto; height:500px">
+              <div id="testButton" data-bs-target="#buttonGroup" style="position:relative; overflow-y:auto; height:500px">
+              <!-- <div id="listgroup-ex" style="position:relative; overflow-y:auto; height:500px"> -->
                 <h4 id="list-item-1">Item 1</h4>
                 <p>{{ text }}</p>
                 <h4 id="list-item-2">Item 2</h4>
@@ -68,6 +73,13 @@ export default {
   name: 'EducationPage',
   data() {
     return {
+      buttons:[
+        {href: "#list-item-1", state: false},
+        {href: "#list-item-2", state: false},
+        {href: "#list-item-3", state: false},
+        {href: "#list-item-4", state: false},
+        {href: "#list-item-5", state: false},
+      ],
       text: `
         Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla
         tempor. Laborum consequat non elit enim exercitation cillum aliqua
@@ -78,6 +90,11 @@ export default {
         commodo non commodo dolor excepteur commodo. Ipsum fugiat ex est consectetur
         ipsum commodo tempor sunt in proident.
       `
+    }
+  },
+  computed: {
+    btnState() {
+      return this.buttons.map(btn => btn.state)
     }
   }
   
